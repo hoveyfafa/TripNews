8f631fb9d37e0df18b16deb9e672a29caeefb403
huangjh <272438151@qq.com>
2018-01-18 10:37:57 +0800

    some change
    change

---
+++ app/src/main/java/com/jiangjh/tripnews/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jiangjh.tripnews;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ *
+ * @author JiaHao.Huang
+ * @date 2018/1/18
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
